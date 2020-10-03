528b91f13a8d77c63e906154dfa3972994fcf068
Shashwat Nigam <mehershashwat@gmail.com>
2020-10-04 02:33:17 +0530

    Fixed front page

--- frontend/src/components/RoleToggle.js
+++ frontend/src/components/RoleToggle.js
@@ -1,32 +1,221 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import StarIcon from '@material-ui/icons/StarBorder';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
     flexGrow: 1,
   },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'baseline',
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    borderTop: `1px solid ${theme.palette.divider}`,
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+    },
+  backGround: {
+    backgroundColor:
+    theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+    backgroundImage: "https://www.qries.com/images/banner_logo.png", 
+    backgroundPosition: 'top',
+    backgroundSize: '100% 100%',
+    backgroundRepeat: 'no-repeat'
+    },
   },
 }));
 
-export default function RoleToggle() {
+const tiers = [
+  {
+    title: 'Community Health Worker',
+    price: '0',
+    description: ['Current cases in assigned area'],
+    buttonText: 'Log in to view',
+    buttonVariant: 'outlined',
+  },
+  {
+    title: 'Public Health official',
+    price: '30',
+    description: [
+      'Region based analytics'
+    ],
+    buttonText: 'Log in to view',
+    buttonVariant: 'outlined',
+  },
+];
+// const footers = [
+//   {
+//     title: 'Company',
+//     description: ['Team', 'History', 'Contact us', 'Locations'],
+//   },
+//   {
+//     title: 'Features',
+//     description: ['View cases in your area'],
+//   },
+//   {
+//     title: 'Resources',
+//     description: ['View analytics for your region'],
+//   },
+//   {
+//     title: 'Legal',
+//     description: ['Privacy policy', 'Terms of use'],
+//   },
+// ];
+
+export default function Pricing() {
   const classes = useStyles();
 
   return (
-    <div class="role-toggle">
-      <Grid justify="center" alignItems="center" container spacing={3}>
-        <Grid item xs={6}>
-          <Button> Government Official </Button>
+    <React.Fragment>
+    <div style ={ { height: "100vh", backgroundImage: "url('https://images.livemint.com/rf/Image-621x414/LiveMint/Period2/2018/02/15/Photos/Processed/healthcareworkers-kfMD--621x414@LiveMint.jpg')",
+    backgroundPosition: 'top',
+    backgroundSize: '80% 100%',
+    backgroundRepeat: 'no-repeat',
+    // opacity: 0.5,
+    zIndex:"-1",
+    } }>
+      <CssBaseline />
+      <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+            Sakhi-Healthcare Tracker
+          </Typography>
+          <nav>
+            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+              About
+            </Link>
+            <Link variant="button" color="textPrimary" href="#" className={classes.link}>
+              Support
+            </Link>
+          </nav>
+        </Toolbar>
+      </AppBar>
+      {/* Hero unit */}
+      <Container maxWidth="sm" component="main" className={classes.heroContent}>
+        <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom style={{fontSize:"10vh"}}>
+          Sakhi
+        </Typography>
+        {/* <Typography variant="h5" align="center" color="textSecondary" component="p">
+          Quickly build an effective pricing table for your potential customers with this layout.
+          It&apos;s built with default Material-UI components with little customization.
+        </Typography> */}
+      </Container>
+      {/* End hero unit */}
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
+          {tiers.map((tier) => (
+            // Enterprise card is full width at sm breakpoint
+            <Grid item key={tier.title} xs={12} sm={6} md={6}>
+              <Card>
+                <CardHeader
+                  title={tier.title}
+                  subheader={tier.subheader}
+                  titleTypographyProps={{ align: 'center' }}
+                  subheaderTypographyProps={{ align: 'center' }}
+                  // action={tier.title === 'Pro' ? <StarIcon /> : null}
+                  className={classes.cardHeader}
+                />
+                <CardContent>
+                  <ul>
+                    {tier.description.map((line) => (
+                      <Typography component="li" variant="subtitle1" align="center" key={line}>
+                        {line}
+                      </Typography>
+                    ))}
+                  </ul>
+                </CardContent>
+                <CardActions>
+                  <Button fullWidth variant={tier.buttonVariant} color="primary">
+                    {tier.buttonText}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
         </Grid>
-        <Grid item xs={6}>
-          <Button> Community Health Worker </Button>
+      </Container>
+      {/* Footer */}
+      {/* <Container maxWidth="md" component="footer" className={classes.footer}>
+        <Grid container spacing={4} justify="space-evenly">
+          {footers.map((footer) => (
+            <Grid item xs={6} sm={3} key={footer.title}>
+              <Typography variant="h6" color="textPrimary" gutterBottom>
+                {footer.title}
+              </Typography>
+              <ul>
+                {footer.description.map((item) => (
+                  <li key={item}>
+                    <Link href="#" variant="subtitle1" color="textSecondary">
+                      {item}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </Grid>
+          ))}
         </Grid>
-      </Grid>
-    </div>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container> */}
+      {/* End footer */}
+    </div>   
+    </React.Fragment>
   );
 }
